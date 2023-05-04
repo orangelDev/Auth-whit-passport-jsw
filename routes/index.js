84d8246ab9611f4ab5e3f7d94901581bca75f5fd
orangeldev <orangeldev@UbuntuOrange.myguest.virtualbox.org>
2023-05-04 02:07:29 +0200

    Implementando login con Passport.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const passport = require('passport');
 
 const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
 const usersRouter = require('./users.router');
 const orderRouter = require('./orders.router');
 const customersRouter = require('./customers.router');
+const authRouter = require('./auth.router');
+
 
 function routerApi(app) {
   const router = express.Router();
@@ -14,6 +17,7 @@ function routerApi(app) {
   router.use('/users', usersRouter);
   router.use('/orders', orderRouter);
   router.use('/customers', customersRouter);
+  router.use('/auth', authRouter);
 }
 
 module.exports = routerApi;
